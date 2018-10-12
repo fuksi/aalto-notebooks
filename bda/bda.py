e7f23fa1a5fd4c2d6641b474a54f8c0447e79d24
Phuc Tran <phucdn2005@gmail.com>
2018-10-12 15:53:10 +0300

    temp

---
+++ bda/bda.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import multivariate_normal
+
+x = np.array([-0.86, -0.30, -0.05, 0.73])
+n = np.array([5, 5, 5, 5])
+y = np.array([0, 1, 3, 5])
